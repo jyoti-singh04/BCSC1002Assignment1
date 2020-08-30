2c203daa9a50acd0442e96227f56a4c6b1ea0383
jyoti-singh04 <jyoti.singh_cs19@gla.ac.in>
2020-08-30 05:54:01 +0530

    add fields for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,10 +1,15 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
- *  File Name : Student.java
+ *  User: Jyoti Singh (jyoti-singh04)
+ *  Date: 30/08/20
+ *  Time: 4:50 AM
+ *  File Name : Book.java
  * */
 package definitions;
 
 public class Student {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private long universityRollNumber;
+    private int booksIssuedbyTheStudent;
 }
