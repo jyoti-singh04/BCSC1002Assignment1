63ee1031473025332de13b3248836dc07ec6e547
jyoti-singh04 <jyoti.singh_cs19@gla.ac.in>
2020-08-30 06:49:14 +0530

    create the menu for FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,15 +1,34 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
- *  File Name : FrontDesk.java
+ *  User: Jyoti Singh (jyoti-singh04)
+ *  Date: 30/08/20
+ *  Time: 4:50 AM
+ *  File Name : Book.java
  * */
 package execution;
 
-import definitions.Book;
+import definitions.Library;
+
+import java.util.Scanner;
 
 public class FrontDesk {
+    private static final int ISSUE_A_NEW_BOOK_FOR_ME = 1;
+    private static final int RETURN_A_NEW_BOOK_FOR_ME = 2;
+    private static final int SHOW_ME_ALL_MY_ISSUES_BOOK_FOR_ME = 3;
+    private static final int EXIT = 4;
+
     public static void main(String[] args) {
-        Book book = new Book();
+        Scanner scanner = new Scanner(System.in);
+        int clientInput;
+        Library myLibrary = new Library();
+        do {
+            System.out.println("-=-=--=-Welcome to front desk-=-=--=-");
+            System.out.println("How may I help you today? ");
+            System.out.println("1. Issue a new book for me. ");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit: ");
+            System.out.println("Enter your choice: ");
+        }
+
     }
 }
