5de213700524bcbd15f460479249863059552222
jyoti-singh04 <jyoti.singh_cs19@gla.ac.in>
2020-08-30 05:44:27 +0530

    modify getter and setter method for Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,7 +1,7 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Jyoti Singh (jyoti-singh04)
+ *  Date: 30/08/20
+ *  Time: 4:50 AM
  *  File Name : Book.java
  * */
 package definitions;
@@ -11,28 +11,27 @@ public class Book {
     private String authorNameOfTheBook;
     private String ISBNNumberOfTheBook;
 
+    public String getBookName() {
+        return bookName;
+    }
 
     public void setBookName(String bookName) {
         this.bookName = bookName;
-    }
-
-    public void setAuthorNameOfTheBook(String authorNameOfTheBook) {
-        this.authorNameOfTheBook = authorNameOfTheBook;
-    }
-
-    public void setISBNNumberOfTheBook(String ISBNNumberOfTheBook) {
-        this.ISBNNumberOfTheBook = ISBNNumberOfTheBook;
-    }
-
-    public String getBookName() {
-        return bookName;
     }
 
     public String getAuthorNameOfTheBook() {
         return authorNameOfTheBook;
     }
 
+    public void setAuthorNameOfTheBook(String authorNameOfTheBook) {
+        this.authorNameOfTheBook = authorNameOfTheBook;
+    }
+
     public String getISBNNumberOfTheBook() {
         return ISBNNumberOfTheBook;
+    }
+
+    public void setISBNNumberOfTheBook(String ISBNNumberOfTheBook) {
+        this.ISBNNumberOfTheBook = ISBNNumberOfTheBook;
     }
 }
